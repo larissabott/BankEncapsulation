5350f91deef581e35b22a9ae07398d274874cf53
[larissa bottefield] <[larissabott3@gmail.com]>
2020-11-05 14:58:58 -0600

    money deposit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thanks, have a great day! Your balance is now {account.GetBalance()}");
+            
+        }
+    }
+}
